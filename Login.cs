fe3de9d3aff983bb67d5e9b1ea0cac8514dd5a17
SudeshMaduwantha <sudeshmaduwantha204@gmail.com>
2024-05-27 10:37:50 +0530

    Add project files.

---
+++ Login.cs
@@ -0,0 +1,20 @@
+namespace RMS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void guna2Button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
