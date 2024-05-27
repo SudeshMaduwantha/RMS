83027c4658a66e6513e34f860cc5c36fa4961210
SudeshMaduwantha <sudeshmaduwantha204@gmail.com>
2024-05-27 18:39:48 +0530

    Category sample addng interface and related things adding

---
+++ SampleAdd.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace RMS
+{
+    partial class SampleAdd
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges1 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges2 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges7 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges8 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges3 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges4 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges5 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges6 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            guna2Panel1 = new Guna.UI2.WinForms.Guna2Panel();
+            label1 = new Label();
+            pictureBox1 = new PictureBox();
+            guna2Panel2 = new Guna.UI2.WinForms.Guna2Panel();
+            btnClose = new Guna.UI2.WinForms.Guna2Button();
+            btnSave = new Guna.UI2.WinForms.Guna2Button();
+            guna2Panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            guna2Panel2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // guna2Panel1
+            // 
+            guna2Panel1.Controls.Add(label1);
+            guna2Panel1.Controls.Add(pictureBox1);
+            guna2Panel1.CustomizableEdges = customizableEdges1;
+            guna2Panel1.Dock = DockStyle.Top;
+            guna2Panel1.FillColor = Color.FromArgb(50, 55, 89);
+            guna2Panel1.Location = new Point(0, 0);
+            guna2Panel1.Name = "guna2Panel1";
+            guna2Panel1.ShadowDecoration.CustomizableEdges = customizableEdges2;
+            guna2Panel1.Size = new Size(453, 81);
+            guna2Panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.Transparent;
+            label1.Font = new Font("Segoe UI", 14F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(93, 29);
+            label1.Name = "label1";
+            label1.Size = new Size(73, 25);
+            label1.TabIndex = 1;
+            label1.Text = "Header";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(12, 12);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(66, 55);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // guna2Panel2
+            // 
+            guna2Panel2.Controls.Add(btnClose);
+            guna2Panel2.Controls.Add(btnSave);
+            guna2Panel2.CustomizableEdges = customizableEdges7;
+            guna2Panel2.Dock = DockStyle.Bottom;
+            guna2Panel2.FillColor = Color.Gainsboro;
+            guna2Panel2.Location = new Point(0, 263);
+            guna2Panel2.Name = "guna2Panel2";
+            guna2Panel2.ShadowDecoration.CustomizableEdges = customizableEdges8;
+            guna2Panel2.Size = new Size(453, 64);
+            guna2Panel2.TabIndex = 1;
+            // 
+            // btnClose
+            // 
+            btnClose.AutoRoundedCorners = true;
+            btnClose.BackColor = Color.Gainsboro;
+            btnClose.BorderRadius = 21;
+            customizableEdges3.TopRight = false;
+            btnClose.CustomizableEdges = customizableEdges3;
+            btnClose.DisabledState.BorderColor = Color.DarkGray;
+            btnClose.DisabledState.CustomBorderColor = Color.DarkGray;
+            btnClose.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            btnClose.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            btnClose.FillColor = Color.FromArgb(50, 55, 89);
+            btnClose.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            btnClose.ForeColor = Color.White;
+            btnClose.Location = new Point(135, 7);
+            btnClose.Name = "btnClose";
+            btnClose.ShadowDecoration.CustomizableEdges = customizableEdges4;
+            btnClose.Size = new Size(117, 45);
+            btnClose.TabIndex = 1;
+            btnClose.Text = "CLOSE";
+            btnClose.Click += btnClose_Click;
+            // 
+            // btnSave
+            // 
+            btnSave.AutoRoundedCorners = true;
+            btnSave.BackColor = Color.Gainsboro;
+            btnSave.BorderRadius = 21;
+            customizableEdges5.TopRight = false;
+            btnSave.CustomizableEdges = customizableEdges5;
+            btnSave.DisabledState.BorderColor = Color.DarkGray;
+            btnSave.DisabledState.CustomBorderColor = Color.DarkGray;
+            btnSave.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            btnSave.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            btnSave.FillColor = Color.FromArgb(241, 85, 126);
+            btnSave.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            btnSave.ForeColor = Color.White;
+            btnSave.Location = new Point(12, 7);
+            btnSave.Name = "btnSave";
+            btnSave.ShadowDecoration.CustomizableEdges = customizableEdges6;
+            btnSave.Size = new Size(117, 45);
+            btnSave.TabIndex = 0;
+            btnSave.Text = "SAVE";
+            btnSave.Click += btnSave_Click;
+            // 
+            // SampleAdd
+            // 
+            AutoScaleMode = AutoScaleMode.None;
+            BackColor = Color.White;
+            ClientSize = new Size(453, 327);
+            Controls.Add(guna2Panel2);
+            Controls.Add(guna2Panel1);
+            ForeColor = Color.FromArgb(64, 64, 64);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "SampleAdd";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "SampleAdd";
+            guna2Panel1.ResumeLayout(false);
+            guna2Panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            guna2Panel2.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+        private Label label1;
+        public Guna.UI2.WinForms.Guna2Panel guna2Panel1;
+        public PictureBox pictureBox1;
+        public Guna.UI2.WinForms.Guna2Button btnSave;
+        public Guna.UI2.WinForms.Guna2Button btnClose;
+        public Guna.UI2.WinForms.Guna2Panel guna2Panel2;
+    }
+}
---
+++ SampleAdd.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RMS
+{
+    public partial class SampleAdd : Form
+    {
+        public SampleAdd()
+        {
+            InitializeComponent();
+        }
+
+        public void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        public void btnSave_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
